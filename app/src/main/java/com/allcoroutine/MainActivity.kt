6b1fd2c5dd2ef7cbafe2c03daa7a9011b16d3c41
arindam_sync <arindam.ghosh@syncrasytech.com>
2021-05-16 00:01:03 +0530

    Network TimeOut with Coroutine

--- app/src/main/java/com/allcoroutine/MainActivity.kt
+++ app/src/main/java/com/allcoroutine/MainActivity.kt
@@ -3,22 +3,20 @@ package com.allcoroutine
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private val RESULT_1="Result#1"
     private val RESULT_2="Result#2"
+    private val JOB_TIMEOUT=1900L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn_click.setOnClickListener {
             CoroutineScope(IO).launch {
-                fakeApiRequest()
+                fakeApiRequestForTimeOut()
             }
 
         }
@@ -34,6 +32,27 @@ class MainActivity : AppCompatActivity() {
         val newText=txt_text.text.toString()+"\n$input"
         txt_text.setText(newText)
     }
+
+
+    private suspend fun fakeApiRequestForTimeOut(){
+        withContext(IO){
+            val job= withTimeoutOrNull(JOB_TIMEOUT) {
+                val result1=getResult1FromApi()
+                setTestOnMainThread("Got $result1")
+
+                val result2=getResult2FromApi()
+                setTestOnMainThread("Got $result2")
+            } // this job wait until complete and moving to next job
+            val job2=launch {
+
+            }
+            if (job==null){
+                val cancelMessage= "Canceling job.... Job took longer then $JOB_TIMEOUT ms"
+                println("debug: $cancelMessage")
+                setTestOnMainThread(cancelMessage)
+            }
+        }
+    }
     private suspend fun fakeApiRequest(){
         val result1=getResult1FromApi() // it wait until the result then set the result to text view.after completing completing this the 2nd fun call
         println("debug: $result1")
@@ -46,7 +65,7 @@ class MainActivity : AppCompatActivity() {
     }
     private suspend fun getResult1FromApi() :String{
         logThread("getResult1FromApi")
-        delay(1000)
+        delay(1000)  // Does not block thread.just suspend the coroutine inside the thread
         return RESULT_1
     }
 
